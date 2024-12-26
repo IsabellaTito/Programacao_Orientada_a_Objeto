a4243f3f767ea6a279a5ad9b324303d8062e6590
IsabellaTito <isabellatito76@gmail.com>
2024-12-26 17:50:39 -0300

    Create testes.cpp

---
+++ Lista_1/testes.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+#include <typeinfo>
+
+
+#include <iostream>
+int main()
+{
+int arr[]{1,2,3,4,5};
+for (auto &a : arr)
+ std::cout << a << " ";
+return 0;
+}
