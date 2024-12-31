c8a5e6cc08a065f3d60fd722e13050655a159eac
IsabellaTito <isabellatito76@gmail.com>
2024-12-30 22:17:06 -0300

    Create Bruxo.cpp

---
+++ Lista_2/Q17/Bruxo.cpp
@@ -0,0 +1,42 @@
+#include "Bruxo.hpp"
+
+Bruxo::Bruxo(string nome, string casa, string feitico){
+    setNome(nome);
+    setCasa(casa);
+    setFeitico(feitico);
+}
+
+string Bruxo::getNome() const{
+    return hp_nome;
+}
+
+void Bruxo::setNome(string nome){
+    hp_nome = nome;
+}
+
+string Bruxo::getCasa() const{
+    return hp_casa;
+}
+
+void Bruxo::setCasa(string casa){
+    if(casa.compare("Grifinoria")!=0 && casa.compare("Sonserina")!=0 && casa.compare("Lufa-lufa")!=0 && casa.compare("Corvinal")!=0){
+        casa = " ";
+    }
+    hp_casa = casa;
+}
+
+string Bruxo::getFeitico() const{
+    return hp_feitico;
+}
+
+void Bruxo::setFeitico(string feitico){
+    hp_feitico = feitico;
+}
+
+void Bruxo::lancarFeitico() const{
+    std::cout<<"Lancando feitico " << getFeitico() << std::endl;
+}
+
+void Bruxo::display() const{
+    std::cout << getNome() << " " << getCasa() << " " << getFeitico() << std::endl;
+}
