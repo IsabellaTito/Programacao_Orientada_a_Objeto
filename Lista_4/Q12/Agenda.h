3e9387e39e34b114202eab768b2317007888f4b0
IsabellaTito <isabellatito76@gmail.com>
2025-01-15 17:32:15 -0300

    Create Agenda.h

---
+++ Lista_4/Q12/Agenda.h
@@ -0,0 +1,29 @@
+#pragma once
+
+#include <string>
+#include <stdexcept>
+#include <iostream>
+#include <format>
+
+#include "Pessoa.h"
+
+using std::string, std::cout, std::endl, std::format;
+
+class Agenda{
+
+    public:
+        Agenda() : qtdePessoas(0){}
+
+        void armazenaPessoa(const string &nome, int idade, float altura);
+        void armazenaPessoa(const Pessoa &p);
+
+        void removePessoa(const string &nome);
+
+        int buscaPessoa(const string &nome) const; // informa em que posição da agenda está a pessoa
+        void imprimePovo() const; // imprime todos os dados de todas as pessoas da agenda
+        void imprimePessoa(int i) const; // imprime os dados da pessoa que está na posição 'i' da agenda
+    
+    private:
+        Pessoa pessoas[10];
+        int qtdePessoas;
+};
