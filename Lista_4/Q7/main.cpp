51f2b6f19775ce8dbce618dc5b4d7bd471aa6ccb
IsabellaTito <isabellatito76@gmail.com>
2025-01-17 10:39:51 -0300

    Create main.cpp

---
+++ Lista_4/Q7/main.cpp
@@ -0,0 +1,18 @@
+#include "CapaBruxo.h"
+
+int main(){
+
+    CapaBruxo c1{1.45,"Grifinoria"}, c2{2.0,"Sonserina"}, c3{1.3,"Corvinal"}, c4{2.1,"Lufa-Lufa"},c5{};
+
+    c1.display();
+
+    c2.display();
+
+    c3.display();
+
+    c4.display();
+
+    c5.display();
+
+    return 0;
+}
