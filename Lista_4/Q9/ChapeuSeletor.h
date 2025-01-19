e7c278b7b805a627eacf63a70114933885d4a0db
IsabellaTito <isabellatito76@gmail.com>
2025-01-19 17:59:38 -0300

    Create ChapeuSeletor.h

---
+++ Lista_4/Q9/ChapeuSeletor.h
@@ -0,0 +1,20 @@
+#pragma once
+#include <string>
+#include <iostream>
+#include <stdlib.h>
+
+using std::string,std::cout,std::endl;
+
+class ChapeuSeletor{
+
+    public:
+
+        void Recepcionar();
+
+        string sortearCasa();
+
+    private:
+
+
+
+};
