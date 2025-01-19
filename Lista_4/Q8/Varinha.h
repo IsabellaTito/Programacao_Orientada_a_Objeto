35b7772f808afc9b8c419d44e311e5da7faab94b
IsabellaTito <isabellatito76@gmail.com>
2025-01-19 17:59:24 -0300

    Create Varinha.h

---
+++ Lista_4/Q8/Varinha.h
@@ -0,0 +1,28 @@
+#pragma once
+
+#include <string>
+#include <iostream>
+
+using std::string, std::cout, std::endl;
+
+class Varinha{
+
+    public:
+        Varinha(string wood = "",string nuc = "",double comp=0):madeira{wood},nucleo{nuc},comprimento{comp}{}
+
+        string getMadeira() const {return madeira;}
+        string getNucleo() const {return nucleo;}
+        double getComp() const {return comprimento;}
+
+        void setMadeira(string madeira){this->madeira=madeira;}
+        void setNucleo(string nuc){this->nucleo=nuc;}
+        void setComp(double comp){this->comprimento=comp;}
+
+        void display(){cout<<"O Bruxo possui uma varinha de madeira "<<madeira<<" com nucleo "<<nucleo<<" e comprimento "<<comprimento<<endl;}
+
+    private:
+        string madeira;
+        string nucleo;
+        double comprimento;
+
+};
