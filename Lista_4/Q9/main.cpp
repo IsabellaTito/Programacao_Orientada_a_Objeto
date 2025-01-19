cec3e8d33d61c65d7e4377a91d761b2142c577e2
IsabellaTito <isabellatito76@gmail.com>
2025-01-19 17:59:45 -0300

    Create main.cpp

---
+++ Lista_4/Q9/main.cpp
@@ -0,0 +1,37 @@
+#include "ChapeuSeletor.h"
+#include <iostream>
+
+using std::cout, std::endl;
+
+int main(){
+
+    ChapeuSeletor c1;
+
+    c1.Recepcionar();
+
+    cout << c1.sortearCasa() <<endl;
+    cout << c1.sortearCasa() <<endl;
+    cout << c1.sortearCasa() <<endl;
+    cout << c1.sortearCasa() <<endl;
+    cout << c1.sortearCasa() <<endl;
+    cout << c1.sortearCasa() <<endl;
+    cout << c1.sortearCasa() <<endl;
+    cout << c1.sortearCasa() <<endl;
+    cout << c1.sortearCasa() <<endl;
+    cout << c1.sortearCasa() <<endl;
+    cout << c1.sortearCasa() <<endl;
+    cout << c1.sortearCasa() <<endl;
+    cout << c1.sortearCasa() <<endl;
+    cout << c1.sortearCasa() <<endl;
+    cout << c1.sortearCasa() <<endl;
+    cout << c1.sortearCasa() <<endl;
+    cout << c1.sortearCasa() <<endl;
+    cout << c1.sortearCasa() <<endl;
+    cout << c1.sortearCasa() <<endl;
+    cout << c1.sortearCasa() <<endl;
+
+
+
+    return 0;
+}
+
