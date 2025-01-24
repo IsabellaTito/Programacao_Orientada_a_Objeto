9cb6f098fd7a2526d63466aa1666ca7c7227ac70
IsabellaTito <isabellatito76@gmail.com>
2025-01-24 09:46:51 -0300

    Create Cliente.h

---
+++ Lista_5/Q14/Cliente.h
@@ -0,0 +1,27 @@
+#pragma once
+
+#include "PessoaFisica.h"
+#include <string>
+
+using std::string;
+
+class Cliente : public PessoaFisica{
+    friend ostream &operator<<(ostream &out, const Cliente &p){
+            out << "Nome da Cliente: " << p.getNome() <<endl;
+            out << "CPF: " << p.getCPF() << endl;
+            return out;
+        }
+    public:
+        Cliente(string nome, string cpf, string end, int telefone): PessoaFisica(nome,cpf){setEndereco(end);setTelefone(telefone);}
+
+        void setEndereco(string end){endereco=end;}
+        void setTelefone(int tele){telefone=tele;}
+
+        string getEndereco()const{return endereco;}
+        int getTelefone()const{return telefone;}
+
+    private:
+        string endereco;
+        int telefone;
+
+};
