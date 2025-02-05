bea79378060d1eedbfe07d7f730bbb9bf71b921f
IsabellaTito <isabellatito76@gmail.com>
2025-02-05 09:47:40 -0300

    Create main.cpp

---
+++ Lista_7/Q7/main.cpp
@@ -0,0 +1,32 @@
+#include "ExpressoHogwarts.h"
+
+int main() {
+  ExpressoHogwarts expresso(100);
+
+  Bruxo harry("Harry","Grifinoria","expecto patronum");
+
+  cout << "Embarcando..." << endl;
+ 
+  expresso << harry;
+  expresso << Bruxo("Rony","Grifinoria","wingardium leviosa");
+  expresso << Bruxo("Hermione","Grifinoria","flipendo");
+
+  expresso << Bruxo("Malfoy","Sonserina","cruciatus");
+
+  cout << expresso;
+
+  cout << "Partindo..." << endl;
+  cout << "Viajando..." << endl;
+ 
+  cout << "Chegando..." << endl;
+  cout << "Desembarcando..." << endl;
+
+  expresso >> "Malfoy";
+  expresso >> harry;
+  expresso >> Bruxo("Rony","Grifinoria","wingardium leviosa");
+  expresso >> Bruxo("Hermione","Grifinoria","flipendo");
+
+  cout << expresso;
+ 
+  return 0;
+}
